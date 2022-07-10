0f67cae1e719b7551af66f3b41a8ebfb6d54e67f
Lulupointu <lucasdelsol@hotmail.fr>
2022-07-10 18:45:59 +0200

    Feat: implement lint

--- lint_no_list/bin/custom_lint.dart
+++ lint_no_list/bin/custom_lint.dart
@@ -2,7 +2,12 @@
 import 'dart:isolate';
 
 import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer_plugin/protocol/protocol_generated.dart';
+import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:source_gen/source_gen.dart';
 
 void main(List<String> args, SendPort sendPort) {
   startPlugin(sendPort, _ExampleLinter());
@@ -12,14 +17,85 @@ void main(List<String> args, SendPort sendPort) {
 class _ExampleLinter extends PluginBase {
   @override
   Stream<Lint> getLints(ResolvedUnitResult unit) async* {
-    // A basic lint that shows at the top of the file.
-    yield Lint(
-      code: 'my_custom_lint_code',
-      message: 'This is the description of our custom lint',
-      // Where your lint will appear within the Dart file.
-      // The following code will make appear at the top of the file (offset 0),
-      // and be 10 characters long.
-      location: unit.lintLocationFromOffset(0, length: 10),
+    final variableDeclarations = <VariableDeclaration>[];
+    unit.unit.visitChildren(
+      RecursiveVariableDeclarationVisitor(
+        onVisitVariableDeclaration: variableDeclarations.add,
+      ),
     );
+
+    final typeChecker = TypeChecker.fromRuntime(List);
+    final variableDeclarationsOfLists = variableDeclarations.where(
+      (variableDeclaration) {
+        final type = variableDeclaration.declaredElement?.type;
+        return type != null && typeChecker.isExactlyType(type);
+      },
+    ).toList();
+
+    for (final variableDeclaration in variableDeclarationsOfLists) {
+      final declaredElement = variableDeclaration.declaredElement;
+      if (declaredElement == null) {
+        return;
+      }
+      final startLintOffset = declaredElement.nameOffset;
+      final lintLength = declaredElement.nameLength;
+      yield Lint(
+        code: 'use_immutable_lists',
+        message: 'Don\'t work with Lists directly, use IList instead',
+        severity: LintSeverity.error,
+        location: unit.lintLocationFromOffset(
+          startLintOffset,
+          length: lintLength,
+        ),
+        getAnalysisErrorFixes: (lint) async* {
+          // [unit] is the [ResolvedUnitResult] given by custom_lints
+          final changeBuilder = ChangeBuilder(session: unit.session);
+
+          await changeBuilder.addDartFileEdit(
+            unit.libraryElement.source.fullName, // Path to the current file
+            (fileEditBuilder) {
+              final expression = variableDeclaration.initializer;
+              if (expression != null) {
+                final startOffset = expression.offset;
+                final endOffset = startOffset + expression.length;
+                fileEditBuilder
+                  // Add "IList(" at the start of the expression
+                  ..addSimpleInsertion(startOffset, 'IList(')
+                  // Add ")" at the end of the expression
+                  ..addSimpleInsertion(endOffset, ')');
+              }
+            },
+          );
+
+          final expression = variableDeclaration.initializer;
+          final sourceChange = changeBuilder.sourceChange;
+          sourceChange.message = "Replace expression with IList($expression)";
+
+          yield AnalysisErrorFixes(
+            lint.asAnalysisError(),
+            fixes: [
+              PrioritizedSourceChange(
+                0,
+                sourceChange,
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
+
+class RecursiveVariableDeclarationVisitor extends RecursiveAstVisitor<void> {
+  RecursiveVariableDeclarationVisitor({
+    required this.onVisitVariableDeclaration,
+  });
+
+  void Function(VariableDeclaration node) onVisitVariableDeclaration;
+
+  @override
+  void visitVariableDeclaration(VariableDeclaration node) {
+    onVisitVariableDeclaration(node);
+    super.visitVariableDeclaration(node);
   }
 }
